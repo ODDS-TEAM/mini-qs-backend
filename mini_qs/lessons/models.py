fe41f9aa9ca0570fe3c8d62f1fea753411cf694a
toptoppy <toptoppy@odds.team>
2020-01-23 10:08:19 +0700

    register lesson to admin :rocket:

--- mini_qs/lessons/models.py
+++ mini_qs/lessons/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
-# Create your models here.
 class Lesson(models.Model):
     number = models.IntegerField()
     title = models.CharField(max_length=50)
     content = models.TextField()
+
+    def __str__(self):
+        return f'{self.number} : {self.title}'
